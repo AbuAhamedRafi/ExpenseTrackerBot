b3339f83b480547935828bf3350bdbaa30e86ed8
AbuAhamedRafi <abuahamedrafi@gmail.com>
2025-11-26 01:39:12 +0600

    feat: Add explicit `makemigrations` and migration file checks to build script

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 rm -f db.sqlite3
+echo "Checking migration files..."
+ls -R expenses/migrations
+echo "Running makemigrations just in case..."
+python manage.py makemigrations expenses
 python manage.py migrate
